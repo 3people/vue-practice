40247747905a3271838a93386b2adb17b7510840
3people <smsn7031@naver.com>
2022-04-19 13:45:21 +0900

    20220419:  숫자야구

--- 숫자야구/NumberBaseball.vue
+++ 숫자야구/NumberBaseball.vue
@@ -16,10 +16,21 @@
 </template>
 
 <script>
+const getNumbers = () => {
+  const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i++) {
+    const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+
+  return array;
+};
+
 export default {
   data() {
     return {
-      answer: ,
+      answer: getNumbers(),
       tries: [],
       value: "",
       result: "",
@@ -27,12 +38,44 @@ export default {
   },
   methods: {
     onSubmitForm() {
-      this.tries.push({
-        try: this.value,
-        result: "홈런",
-      });
-      this.value = "";
-      this.$refs.answer.focus();
+      if (this.value === this.answer.join("")) {
+        this.tries.push({
+          try: this.value,
+          result: "홈런",
+        });
+        this.result = "홈런";
+        this.value = "";
+        this.answer = getNumbers();
+        this.tries = [];
+        this.$refs.answer.focus();
+      } else {
+        if (this.tries.length >= 9) {
+          this.result = `10번 넘게 틀려서 실패! 답은 ${this.answer.join(
+            ","
+          )}였습니다!`;
+          alert("게임을 다시 시작합니다.");
+          this.value = "";
+          this.answer = getNumbers();
+          this.tries = [];
+          this.$refs.answer.focus();
+        }
+        let strike = 0;
+        let ball = 0;
+        const answerArray = this.value.split("").map((v) => parseInt(v));
+        for (let i = 0; i < 4; i++) {
+          if (answerArray[i] === this.answer[i]) {
+            strike++;
+          } else if (this.answer.includes(answerArray[i])) {
+            ball++;
+          }
+        }
+        this.tries.push({
+          try: this.value,
+          result: `${strike} 스트라이크, ${ball} 볼`,
+        });
+        this.value = "";
+        this.$refs.answer.focus();
+      }
     },
   },
 };
